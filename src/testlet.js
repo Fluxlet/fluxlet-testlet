db80ecf9835c18588427c85ac631d116e680140f
Mark Gibson <mgibson@adaptavist.com>
2015-10-13 09:20:50 +0100

    Re-ident

--- src/testlet.js
+++ src/testlet.js
@@ -90,18 +90,18 @@ export let waitUntil
 //     expect(spy.action.someCalculation.then).toHaveBeenCalled();
 //
 export const spy = {
-    action: {},
-    calculation: {},
-    sideEffect: {}
+  action: {},
+  calculation: {},
+  sideEffect: {}
 }
 
 // Wrap all named functions or when & then of fluxlet conditionals in a spy and
 // register the spy
 export function spyOn(type, namedConditionals) {
-    return Object.keys(namedConditionals).reduce((ret, name) => {
-        spy[type][name] = ret[name] = fluxletSpy(type, name, namedConditionals[name])
-        return ret
-    }, {})
+  return Object.keys(namedConditionals).reduce((ret, name) => {
+    spy[type][name] = ret[name] = fluxletSpy(type, name, namedConditionals[name])
+    return ret
+  }, {})
 }
 
 // A no-operation mock action
@@ -129,13 +129,13 @@ export const mockAction = () => state => state
 //     })
 //
 export function initTestlet(opts = {}) {
-    createSpy = opts.createSpy || ((desc, fn) => fn)
-    instance = undefined
-    Object.keys(spy).forEach(key => spy[key] = {})
-    given = createGiven()
-    when = undefined
-    then = unavailableFn("then")
-    waitUntil = unavailableFn("waitUntil")
+  createSpy = opts.createSpy || ((desc, fn) => fn)
+  instance = undefined
+  Object.keys(spy).forEach(key => spy[key] = {})
+  given = createGiven()
+  when = undefined
+  then = unavailableFn("then")
+  waitUntil = unavailableFn("waitUntil")
 }
 
 // ## Internals
@@ -148,137 +148,137 @@ let createSpy
 
 // ### Given
 function createGiven() {
-    if (!instance) {
-        return {
-            // Create a fluxlet for testing against
-            fluxlet() {
-                instance = fluxlet()
-                // Register a side effect that gathers the state and end
-                // states that are passed to the side effects after the
-                // action and calculation fns have been called in a dispatch.
-                .sideEffects({
-                    gatherState: (...args) => {
-                        then = createThen(...args)
-                    }
-                })
+  if (!instance) {
+    return {
+      // Create a fluxlet for testing against
+      fluxlet() {
+        instance = fluxlet()
+        // Register a side effect that gathers the state and end
+        // states that are passed to the side effects after the
+        // action and calculation fns have been called in a dispatch.
+        .sideEffects({
+          gatherState: (...args) => {
+            then = createThen(...args)
+          }
+        })
 
-                given = createGiven()
-                waitUntil = createWaitUntil()
+        given = createGiven()
+        waitUntil = createWaitUntil()
 
-                return given
-            },
+        return given
+      },
 
-            // Other methods will throw an error if called before the fluxlet
-            // has been created
-            logging: unavailableFn("logging"),
-            validator: unavailableFn("validator"),
-            state: unavailableFn("state"),
-            actions: unavailableFn("actions"),
-            calculations: unavailableFn("calculations"),
-            sideEffects: unavailableFn("sideEffects")
-        }
-    } else {
-        return {
-            // Throw an error if the fluxlet has already been created
-            fluxlet() {
-                throw new Error("a fluxlet has already been created")
-            },
-
-            // Adjust logging levels
-            logging(categories) {
-                instance.logging(categories)
-                return given
-            },
-
-            // Set the state validator function, this should be called before
-            // setting the initial state
-            validator(validator) {
-                instance.validator(validator)
-                return given
-            },
-
-            // Set the initial state.
-            // Generally called in a beforeEach with the default state, but may
-            // also be called in individual test cases with a function to
-            // override parts of the default state.
-            state(initialState) {
-                instance.state(initialState)
-                const state = instance.debug.state()
-                then = createThen(state, state)
-                return given
-            },
-
-            // Register actions for testing
-            actions(namedActions) {
-                instance.actions(spyOn("action", namedActions))
-                when = createWhen()
-                return given
-            },
-
-            // Register calculations for testing
-            calculations(namedCalculations) {
-                instance.calculations(spyOn("calculation", namedCalculations))
-                return given
-            },
-
-            // Register side effects for testing
-            sideEffects(namedSideEffects) {
-                instance.sideEffects(spyOn("sideEffect", namedSideEffects))
-                return given
-            }
-        }
+      // Other methods will throw an error if called before the fluxlet
+      // has been created
+      logging: unavailableFn("logging"),
+      validator: unavailableFn("validator"),
+      state: unavailableFn("state"),
+      actions: unavailableFn("actions"),
+      calculations: unavailableFn("calculations"),
+      sideEffects: unavailableFn("sideEffects")
     }
+  } else {
+    return {
+      // Throw an error if the fluxlet has already been created
+      fluxlet() {
+        throw new Error("a fluxlet has already been created")
+      },
+
+      // Adjust logging levels
+      logging(categories) {
+        instance.logging(categories)
+        return given
+      },
+
+      // Set the state validator function, this should be called before
+      // setting the initial state
+      validator(validator) {
+        instance.validator(validator)
+        return given
+      },
+
+      // Set the initial state.
+      // Generally called in a beforeEach with the default state, but may
+      // also be called in individual test cases with a function to
+      // override parts of the default state.
+      state(initialState) {
+        instance.state(initialState)
+        const state = instance.debug.state()
+        then = createThen(state, state)
+        return given
+      },
+
+      // Register actions for testing
+      actions(namedActions) {
+        instance.actions(spyOn("action", namedActions))
+        when = createWhen()
+        return given
+      },
+
+      // Register calculations for testing
+      calculations(namedCalculations) {
+        instance.calculations(spyOn("calculation", namedCalculations))
+        return given
+      },
+
+      // Register side effects for testing
+      sideEffects(namedSideEffects) {
+        instance.sideEffects(spyOn("sideEffect", namedSideEffects))
+        return given
+      }
+    }
+  }
 }
 
 // ### When
 function createWhen() {
-    return instance.debug.dispatchers()
+  return instance.debug.dispatchers()
 }
 
 // ### Then
 function createThen(...args) {
-    return fn => fn(...args)
+  return fn => fn(...args)
 }
 
 // ### WaitUntil
 function createWaitUntil() {
-    return when => ({
-        then(then) {
-            instance.sideEffects({
-                testExpectedStateChange: { when, then }
-            })
-        }
-    })
+  return when => ({
+    then(then) {
+      instance.sideEffects({
+        testExpectedStateChange: { when, then }
+      })
+    }
+  })
 }
 
 // ### API Helper
 // Create the stubs for function unavailable prior to fluxlet creation
 function unavailableFn(fnName) {
-    return () => {
-        throw new Error(`Cannot call '${fnName}' until a fluxlet has been created`)
-    }
+  return () => {
+    throw new Error(`Cannot call '${fnName}' until a fluxlet has been created`)
+  }
 }
 
 // ### Spies
 // Create the spy for an individual action, calculation or side-effect
 function fluxletSpy(type, name, fnOrCond) {
 
-    function spy(fn, suffix) {
-        return typeof fn === 'function'
-          ? createSpy(suffix ? `${type} ${name}.${suffix}` : `${type} ${name}`, fn)
-          : fn
-    }
+  function spy(fn, suffix) {
+    return typeof fn === 'function'
+    ? createSpy(suffix ? `${type} ${name}.${suffix}` : `${type} ${name}`, fn)
+    : fn
+  }
 
-    function spyObj(cond) {
-      return Object.keys(cond).reduce((ret, key) => {
-        ret[key] = spy(cond[key], key)
-        return ret
-      }, {})
-    }
+  function spyObj(cond) {
+    return Object.keys(cond).reduce((ret, key) => {
+      ret[key] = spy(cond[key], key)
+      return ret
+    }, {})
+  }
 
-    if (fnOrCond && typeof fnOrCond === "object") {
-        return spyObj(fnOrCond)
-    } else {
-        return spy(fnOrCond)
-    }
+  if (fnOrCond && typeof fnOrCond === "object") {
+    return spyObj(fnOrCond)
+  } else {
+    return spy(fnOrCond)
+  }
 }
